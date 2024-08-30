cb9ebbfb7c11637b29c7fffc45ed9cdf3dc064db
eternalUnion <dnzbyk12345@gmail.com>
2024-08-31 00:17:42 +0200

    Squashed all commits to reduce repo size

---
+++ AngryCatalogEditor/AngryCombiner/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace AngryCombiner
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("File path: ");
+			string path = Console.ReadLine();
+
+			if (path.StartsWith('"'))
+				path = path.Substring(1, path.Length - 2);
+
+			if (!File.Exists(path))
+			{
+				Console.WriteLine("File not found");
+				Console.ReadKey();
+				return;
+			}
+
+			if (Path.GetExtension(path) != ".angry1")
+			{
+				Console.WriteLine("Is not a .angry1 file");
+				Console.ReadKey();
+				return;
+			}
+
+			string fname = Path.GetFileNameWithoutExtension(path);
+			string fdir = Path.GetDirectoryName(path);
+
+			string targetFile = Path.Combine(fdir, fname + ".angry");
+			if (File.Exists(targetFile))
+			{
+				Console.WriteLine("Target file already exists");
+				Console.ReadKey();
+			}
+
+			using (var fs = File.Open(targetFile, FileMode.Create, FileAccess.Write))
+			{
+				for (int partedFile = 1; ; partedFile++)
+				{
+					string partedPath = Path.Combine(fdir, fname + $".angry{partedFile}");
+					if (!File.Exists(partedPath))
+						break;
+
+					byte[] partedBytes = File.ReadAllBytes(partedPath);
+					fs.Write(partedBytes, 0, partedBytes.Length);
+				}
+			}
+		}
+	}
+}
---
+++ AngryCatalogEditor/AngryParter/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace AngryParter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("File path: ");
+            string path = Console.ReadLine();
+
+            if (path.StartsWith('"'))
+                path = path.Substring(1, path.Length - 2);
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File not found");
+                Console.ReadKey();
+                return;
+            }
+
+            string fname = Path.GetFileNameWithoutExtension(path);
+            string fdir = Path.GetDirectoryName(path);
+
+            int partNum = 1;
+            int partSize = 1024 * 1024 * 24;
+            using (BinaryReader stream = new BinaryReader(File.Open(path, FileMode.Open, FileAccess.Read)))
+            {
+                Stream baseStr = stream.BaseStream;
+                long remaining = baseStr.Length;
+
+                byte[] buff = new byte[81920];
+                int blockSize = 81920;
+
+                while (remaining > 0)
+                {
+                    int toRead = (remaining > partSize) ? partSize : (int)remaining;
+                    remaining -= toRead;
+
+                    using (FileStream bw = File.Create(Path.Combine(fdir, fname + $".angry{partNum++}")))
+                    {
+                        while (toRead > 0)
+                        {
+                            int blockToRead = (toRead > blockSize) ? blockSize : toRead;
+                            toRead -= blockToRead;
+
+                            stream.Read(buff, 0, blockToRead);
+                            bw.Write(buff, 0, blockToRead);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
