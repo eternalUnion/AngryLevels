8e872d055721548119eda86e51da39d9f30dc544
eternalUnion <dnzbyk12345@gmail.com>
2026-02-12 21:46:21 +0100

    Initial web page

---
+++ AngryCatalogEditor/AngryCatalogEditor.GUI/Pages/ModifyLevel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AngryCatalogEditor.GUI.Pages
+{
+    public class ModifyLevelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
