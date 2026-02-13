6cc74f563d255a28f96dee99905fa848209d8b80
eternalUnion <dnzbyk12345@gmail.com>
2026-02-13 23:04:45 +0100

    Modify level list implementation

---
+++ AngryCatalogEditor/AngryCatalogEditor.GUI/AngryCatalog.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+
+namespace AngryCatalogEditor.GUI
+{
+	public class BundleInfo
+	{
+		public class UpdateInfo
+		{
+			public long Date { get; set; }
+			public string Hash { get; set; }
+			public string Message { get; set; }
+		}
+
+		public class LevelInfo
+		{
+			public string LevelName { get; set; }
+			public string LevelId { get; set; }
+
+			public bool isSecretLevel { get; set; }
+			public List<string> requiredCompletedLevelIdsForUnlock;
+
+			public int secretCount { get; set; }
+
+			public bool levelChallengeEnabled { get; set; }
+			public string levelChallengeText { get; set; }
+
+			public List<string> requiredDllNames;
+		}
+
+		public string Name { get; set; }
+		public string Author { get; set; }
+		public int Size { get; set; }
+		public string Guid { get; set; }
+		public string Hash { get; set; }
+		public string ThumbnailHash { get; set; }
+
+		public bool Locked { get; set; }
+		public List<string> Parts;
+		public long LastUpdate { get; set; }
+		public List<UpdateInfo> Updates;
+
+		public List<LevelInfo> Levels;
+	}
+
+	public class LevelCatalog
+	{
+		public List<BundleInfo> Levels;
+	}
+
+	public class ScriptInfo
+	{
+		public string FileName { get; set; }
+		public string Hash { get; set; }
+		public int Size { get; set; }
+		public List<string> Updates;
+	}
+
+	public class ScriptCatalog
+	{
+		public List<ScriptInfo> Scripts;
+	}
+
+
+
+	public static class AngryCatalogHandler
+	{
+		private static LevelCatalog? _catalog = null;
+
+		private static string? _rootPath = null;
+		public static string? rootPath
+		{
+			get
+			{
+				if (_rootPath != null)
+					return _rootPath;
+
+				string path = Directory.GetCurrentDirectory();
+				while (Directory.Exists(path) && !Directory.Exists(Path.Combine(path, ".git")))
+				{
+					var parent = Directory.GetParent(path);
+					if (parent == null)
+					{
+						_rootPath = null;
+						return null;
+					}
+
+					path = parent.FullName;
+				}
+
+				_rootPath = path;
+
+				return _rootPath;
+			}
+		}
+
+		private static string? _catalogPath = null;
+		public static string? catalogPath
+		{
+			get
+			{
+				if (_catalogPath != null)
+					return _catalogPath;
+
+				if (rootPath != null)
+					_catalogPath = Path.Combine(rootPath, "V2", "LevelCatalog.json");
+
+				return _catalogPath;
+			}
+		}
+
+		public static bool TryGetCatalog(out LevelCatalog catalog)
+		{
+			if (_catalog != null)
+			{
+				catalog = _catalog;
+				return true;
+			}
+
+			if (catalogPath == null)
+			{
+				catalog = null;
+				return false;
+			}
+
+			_catalog = catalog = JsonConvert.DeserializeObject<LevelCatalog>(File.ReadAllText(catalogPath));
+			return true;
+		}
+	}
+}
---
+++ AngryCatalogEditor/AngryCatalogEditor.GUI/Pages/CatalogApi.cshtml.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace AngryCatalogEditor.GUI.Pages
+{
+    public class CatalogApiModel : PageModel
+    {
+		static bool IsGUID(string guid)
+		{
+			if (guid == null || guid.Length != 32)
+				return false;
+
+			foreach (char c in guid)
+			{
+				if (c >= '0' && c <= '9')
+					continue;
+				if (c >= 'a' && c <= 'f')
+					continue;
+				if (c >= 'A' && c <= 'F')
+					continue;
+
+				return false;
+			}
+
+			return true;
+		}
+
+
+
+		public IActionResult OnGet()
+        {
+            return BadRequest("No handler specified.");
+        }
+
+        public IActionResult OnGetCatalog()
+        {
+            if (!AngryCatalogHandler.TryGetCatalog(out LevelCatalog catalog))
+                return BadRequest("Catalog not found.");
+
+            return Content(JsonConvert.SerializeObject(catalog), "application/json");
+		}
+
+        public IActionResult OnGetThumbnail()
+        {
+            string? guid = Request.Query["guid"];
+            if (string.IsNullOrEmpty(guid))
+                return BadRequest("Missing GUID.");
+
+			if (!IsGUID(guid))
+				return BadRequest("Invalid GUID.");
+
+			string? rootDir = AngryCatalogHandler.rootPath;
+			if (rootDir == null)
+				return BadRequest("Could not locate project root.");
+
+			return File(System.IO.File.Open(Path.Combine(rootDir, "Levels", guid, "thumbnail.png"), FileMode.Open, FileAccess.Read), "image/png");
+        }
+    }
+}
