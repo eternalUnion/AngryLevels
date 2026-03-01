aabceaad50fd8d32306fa531d87499eb6b31a598
eternalUnion <dnzbyk12345@gmail.com>
2026-03-01 13:43:28 +0100

    Bump version

--- AngryCatalogEditor/AngryCatalogEditor.GUI/AppConfig.cs
+++ AngryCatalogEditor/AngryCatalogEditor.GUI/AppConfig.cs
@@ -2,8 +2,8 @@
 {
 	public static class AppConfig
 	{
-		public static readonly Version AngryLevelsVersion = new Version("6.2");
-		public static readonly string AngryLevelsName = "Revamp Update";
+		public static readonly Version AngryLevelsVersion = new Version("7.0");
+		public static readonly string AngryLevelsName = "Fraud Update";
 
 		public static readonly string RootURL = "https://localhost:7129";
 		public static readonly string ClientID = "Ov23limrRilIV88VHT5B";
