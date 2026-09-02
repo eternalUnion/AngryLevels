a7bc4bec6763751ebada8bceed8cf05bc8d9f77f
eternalUnion <dnzbyk12345@gmail.com>
2026-09-02 09:36:40 +0200

    Fixed parted files with at least 10 parts getting sorted incorrectly

--- AngryCatalogEditor/AngryCatalogEditor.GUI/AppConfig.cs
+++ AngryCatalogEditor/AngryCatalogEditor.GUI/AppConfig.cs
@@ -2,7 +2,7 @@
 {
 	public static class AppConfig
 	{
-		public static readonly Version AngryLevelsVersion = new Version("7.2.0");
+		public static readonly Version AngryLevelsVersion = new Version("7.2.1");
 		public static readonly string AngryLevelsName = "Fraud Update";
 
 		public static readonly string RootURL = "https://localhost:7129";
